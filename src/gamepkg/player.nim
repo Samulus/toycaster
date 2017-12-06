1b5d0068dca89c6e2e6bd1dbe1202de947e0e48e
Samuel Vargas <svargas@mozilla.com>
2017-12-06 09:12:52 -0700

    Add ability to rotate

--- src/gamepkg/player.nim
+++ src/gamepkg/player.nim
@@ -4,23 +4,28 @@
 #
 
 import glm
+import math
 import units
 import options
 
 const
     WalkingSpeed: Meter = 1.4
+    RotateSpeed: Meter = 1.4
+    UpAxis = vec3f(0, 1, 0)
 
 type Player = ref object of RootObj
     position: Vec3f
     forward: Vec3f
-    rotation: Vec3f
+    rotation: Quatf
     direction: Option[Direction]
+    theta: float
 
 proc ctor*(): Player =
     result = Player();
     result.position = vec3f(0, 0, 0)
     result.forward = vec3f(0, 0, -1)
-    result.rotation = vec3f(0, 0, 0)
+    result.rotation = quatf(0,0,0,1)
+    result.theta = 0
 
 method position*(this: Player): Vec3f {.base.} =
     this.position
@@ -39,6 +44,12 @@ method update*(this: Player, dt: float): void {.base.} =
             this.position.z += WalkingSpeed * dt
         of Backward:
             this.position.z -= WalkingSpeed * dt
+        of Left:
+            this.theta -= degToRad(1.0)
+            this.rotation = quatf(UpAxis, this.theta)
+        of Right:
+            this.theta += degToRad(1.0)
+            this.rotation = quatf(UpAxis, this.theta)
         else:
             discard
     this.direction = none(Direction)
